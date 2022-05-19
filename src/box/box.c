93d442719591e5a9f728ec0ebcfe8b5ab4215de9
fl0pp5 <fl0pp5@mail.ru>
2022-05-19 23:41:33 +0300

    EDIT:     - src/box/box.h     - src/box/box.c
        CHANGES:
            ADDED (src/box/box.h):
                - enum `BoxCodes` { B_OK, ..., B_REALLOC_ERROR }
                - struct `box_t`:
                    - void   *data     - contains user data
                    - size_t len       - array cells in use a.k.a. last index
                    - size_t cap       - actual number of cells
                    - size_t _mem_used - `data` size in bytes
    
                - box_t* new_box(...) {...}:
                    - description:
                        creates a new `box_t` object and initializes fields
                    - return:
                        `box_t*`
    
                - int inc_box(...) {...}:
                    - description:
                        increases `box_t.data` size
                    - return:
                        `BoxCodes`
    
                - void dealloc_box(...) {...}:
                    - description:
                        frees memory from `box_t` object
    
                - int box_get(...) {...}:
                    - description:
                        gets element of array (`box_t.data`) by id
                    - return:
                        `BoxCodes`
    
                - int box_add(...) {...}:
                    - description:
                        adds a new element to the end of the array (`box_t.data`)
                    - return:
                        `BoxCodes`
    
                - int box_del(...) {...}:
                    - description:
                        removes element of array (`box_t.data`) by id
                    - return:
                        `BoxCodes`
    
            ADDED (src/box/box.c):
                - implementation of methods from `src/box/box.h`

---
+++ src/box/box.c
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+
+#include "box.h"
+
+
+box_t *
+new_box(size_t n, size_t item_size) {
+
+    box_t *box = (box_t *) malloc(sizeof(box_t));
+
+    if (box == NULL) {
+        return NULL;
+    }
+
+    if ((box->data = malloc(n * item_size)) == NULL) {
+        dealloc_box(box);
+
+        return NULL;
+    }
+
+    box->cap       = n;
+    box->len       = 0;
+    box->_mem_used = n * item_size;
+
+    return box;
+}
+
+int
+inc_box(box_t *box) {
+    void *ptr = realloc(box->data, box->_mem_used * 2);
+
+    if (ptr == NULL) {
+        return B_REALLOC_ERROR;
+    }
+
+    box->data      = ptr;
+    box->cap       *= 2;
+    box->_mem_used *= 2;
+
+    return B_OK;
+}
+
+void
+dealloc_box(box_t *box) {
+    if (box->data != NULL) {
+        free(box->data);
+    }
+    free(box);
+}
+
+
+int
+box_get(box_t *box, size_t index, void *storage, void (*get_func)(box_t*, size_t, void*)) {
+    if (index < 0 || index >= box->len) {
+        return B_INDEX_OUT_OF_RANGE;
+    }
+
+    get_func(box, index, storage);
+
+    return B_OK;
+}
+
+int
+box_add(box_t *box, void *value, void (*add_func)(box_t*, void*)) {
+    if (box->len < box->cap) {
+        add_func(box, value);
+        box->len++;
+        return B_OK;
+    }
+    return B_CAP_OVERFLOW;
+}
+
+int
+box_del(box_t *box, size_t index, size_t step, void (*del_func)(void*)) {
+    void *ptr;
+
+    if (index < 0 || index >= box->len || box->len <= 0) {
+        return B_INDEX_OUT_OF_RANGE;
+    }
+
+    ptr = box->data + index * step;
+    for (; index < box->len; index++, ptr += step) {
+        del_func(ptr);
+    }
+    box->len--;
+
+    return B_OK;
+}
--- src/box/box.h
+++ src/box/box.h
@@ -1,4 +1,41 @@
 #ifndef BOX_H
 #define BOX_H
 
+#include <stdint.h>
+
+
+enum BoxCodes {
+    B_OK,
+    B_CAP_OVERFLOW,
+    B_INDEX_OUT_OF_RANGE,
+    B_REALLOC_ERROR,
+};
+
+typedef struct {
+    void    *data;
+    size_t  len;
+    size_t  cap;
+
+    size_t  _mem_used;
+} box_t;
+
+box_t *
+new_box(size_t n, size_t item_size);
+
+int
+inc_box(box_t *box);
+
+void
+dealloc_box(box_t *box);
+
+int
+box_get(box_t *box, size_t index, void *storage, void (*get_func)(box_t*, size_t, void*));
+
+int
+box_add(box_t *box, void *value, void (*add_func)(box_t*, void*));
+
+int
+box_del(box_t *box, size_t index, size_t step, void (*del_func)(void*));
+
 #endif //BOX_H
+
